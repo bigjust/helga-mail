834009db689bc7a2a925585077e9c44fe1f54522
Justin Caratzas <bigjust@lambdaphil.es>
2017-09-13 00:35:34 -0400

    initial commit

---
+++ README.rst
@@ -0,0 +1,24 @@
+helga-mail
+===========
+
+Cause sometimes bouncers aren't enough, or reliable. For those times
+when we want to send a message to another user before we forget,
+here's helga-mail.::
+
+  <bigjust> ,mail justin Hey Buddy, welcome back from your trip!
+  <helga> message saved for justin.
+
+  Later...
+
+  *justin joins*
+  <helga> justin, message from bigjust: Hey Buddy, welcome back from
+  your trip!
+  <justin> dawwww
+
+
+License
+-------
+
+Copyright (c) 2017 Justin Caratzas
+
+GPLv3 Licensed
---
+++ helga_mail.py
@@ -0,0 +1,58 @@
+import smokesignal
+
+from helga.db import db
+from helga.plugins import command
+
+
+@command('mail', help='send message to offline user, usage: mail <nick> <message>')
+def mail(client, channel, nick, message, cmd, args):
+
+    if len(args) < 2:
+        return u'usage: mail <nick> <message>'
+
+    recipient = args[0]
+    msg = ' '.join(args[1:])
+
+    db.mail.insert({
+        'sender': nick,
+        'recipient': recipient,
+        'message': msg,
+        'received': False,
+        'channel': channel,
+    })
+
+def check_messages(client, nick):
+    """
+    Check for messages, output if any.
+    """
+
+    unread_messages = db.mail.find({
+        'recipient': nick,
+        'received': False,
+    })
+
+    for message in unread_messages:
+        client.msg(
+            message['channel'],
+            '{}, message from {}: {}'.format(
+                message['recipient'],
+                message['sender'],
+                message['message'],
+            )
+        )
+
+        db.mail.update_one({
+            '_id': message['_id'],
+        }, {
+            '$set': {'received': True},
+        })
+
+@smokesignal.on('user_joined')
+def user_joined(client, user, channel):
+
+    check_messages(client, nick)
+
+@smokesignal.on('user_rename')
+def user_rename(client, oldname, newname):
+
+    check_messages(client, newname)
